a7a413b32fb2ada1dc71e089157eb42cdf6ae975
aep <aepsaprul18@gmail.com>
2022-04-18 14:49:13 +0700

    update field user, add font google css

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->integer('karyawan_id')->nullable();
+            $table->string('role', 20)->nullable();
             $table->timestamps();
         });
     }
